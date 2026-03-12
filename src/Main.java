8d908e1aebd5059831e77f2d0d85fe718918ef34
Seb <rudas42299@gmail.com>
2026-03-12 14:22:31 -0500

    codigo de dados

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //codigo de Dados//
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+       Random aleatorio = new Random();
+      int d1, d2;
+      int to1 = 0, to2 = 0;
+      int sumad1 = 0, sumad2 = 0;
+      do{
+          d1 = aleatorio.nextInt(6)+1;
+          System.out.println("El dado 1 a dado: " + d1);
+          d2 = aleatorio.nextInt(6)+1;
+          System.out.println("El dado 2 a dado: " + d2);
+          sumad1+=d1;
+          sumad2+=d2;
+
+      }while(d1!=d2);
+      {
+        System.out.println("la suma del dado 1 es: "+sumad1);
+        System.out.println("la suma del dado 2 es: "+sumad2);
+       if (sumad1>sumad2){
+           System.out.println("El dado 1 es el ganado con: "+ sumad1 + "puntos");
+       } else if (sumad1<sumad2) {
+           System.out.println("El dado 2 es el ganado con: "+ sumad2 + "puntos");
+       }
+      }
+
+      //Codigo de juego de adivinar un numero//
+
+
+
+
+
+
+
+
+
+
     }
 }
